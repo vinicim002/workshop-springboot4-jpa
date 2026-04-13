bfa165b125f4ae0f1795f0d49c63194e9e4ce922
Vinicius Silva <viniciusvilanova7@gmail.com>
2026-04-13 20:40:20 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/vinicius/course/entities/Product.java
+++ src/main/java/com/vinicius/course/entities/Product.java
@@ -1,5 +1,6 @@
 package com.vinicius.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serial;
@@ -28,6 +29,9 @@ public class Product implements Serializable {
             inverseJoinColumns = @JoinColumn(name = "category_id")
     )
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product(){}
 
@@ -81,6 +85,15 @@ public class Product implements Serializable {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders() {
+        Set<Order> set = new HashSet<>();
+        for (OrderItem x : items) {
+            set.add(x.getOrder());
+        }
+        return set;
     }
 
     @Override
