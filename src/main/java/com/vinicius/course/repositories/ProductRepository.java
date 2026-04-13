ccfaf4d33313e27c6c72d66b9f2346c8b1173d70
Vinicius Silva <viniciusvilanova7@gmail.com>
2026-04-13 17:38:39 -0300

    Product

---
+++ src/main/java/com/vinicius/course/repositories/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.vinicius.course.repositories;
+
+import com.vinicius.course.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
