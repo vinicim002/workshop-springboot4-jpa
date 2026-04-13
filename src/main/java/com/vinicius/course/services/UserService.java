edfe47c6fb725fab09ab23833a1155549d08ded6
Vinicius Silva <viniciusvilanova7@gmail.com>
2026-04-13 14:18:44 -0300

    Service layer, component registration

---
+++ src/main/java/com/vinicius/course/services/UserService.java
@@ -0,0 +1,24 @@
+package com.vinicius.course.services;
+
+import com.vinicius.course.entities.User;
+import com.vinicius.course.repositories.UseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UseRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
