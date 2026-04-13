eefc37e2968f4071f5d7ddabddfd08ff22ce39cb
Vinicius Silva <viniciusvilanova7@gmail.com>
2026-04-13 17:18:12 -0300

    Category

--- src/main/java/com/vinicius/course/config/TestConfig.java
+++ src/main/java/com/vinicius/course/config/TestConfig.java
@@ -1,8 +1,10 @@
 package com.vinicius.course.config;
 
+import com.vinicius.course.entities.Category;
 import com.vinicius.course.entities.Order;
 import com.vinicius.course.entities.User;
 import com.vinicius.course.entities.enums.OrderStatus;
+import com.vinicius.course.repositories.CategoryRepository;
 import com.vinicius.course.repositories.OrderRepository;
 import com.vinicius.course.repositories.UseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,8 +24,17 @@ public class TestConfig implements CommandLineRunner{
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public void run(String... args) throws Exception {
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
