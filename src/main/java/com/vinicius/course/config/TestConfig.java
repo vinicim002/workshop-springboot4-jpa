039902bf735027e2adbbeb93263da36b9047dd02
Vinicius Silva <viniciusvilanova7@gmail.com>
2026-04-13 19:59:24 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/vinicius/course/config/TestConfig.java
+++ src/main/java/com/vinicius/course/config/TestConfig.java
@@ -1,14 +1,8 @@
 package com.vinicius.course.config;
 
-import com.vinicius.course.entities.Category;
-import com.vinicius.course.entities.Order;
-import com.vinicius.course.entities.Product;
-import com.vinicius.course.entities.User;
+import com.vinicius.course.entities.*;
 import com.vinicius.course.entities.enums.OrderStatus;
-import com.vinicius.course.repositories.CategoryRepository;
-import com.vinicius.course.repositories.OrderRepository;
-import com.vinicius.course.repositories.ProductRepository;
-import com.vinicius.course.repositories.UseRepository;
+import com.vinicius.course.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -31,6 +25,9 @@ public class TestConfig implements CommandLineRunner{
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -65,5 +62,12 @@ public class TestConfig implements CommandLineRunner{
 
         useRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
+        OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+        OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+        OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+        OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
+        orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
     }
 }
---
+++ src/main/java/com/vinicius/course/repositories/OrderItemRepository.java
@@ -0,0 +1,7 @@
+package com.vinicius.course.repositories;
+
+import com.vinicius.course.entities.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+}
